db92e14ff94f7d4f3fd4c84b953d98e5b02348de
sanku <sankukei@gmail.com>
2024-11-11 16:40:30 +0100

    makefill

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME = libft.a
+
+COMP = cc
+
+FLAGS = -Wall -Wextra -Werror -I ./
+
+SRC = ft_isprint.c
+
+OBJS = ${SRC:.c=.o}
+
+.c.o:
+	${COMP} ${FLAGS} -c $< -o ${<:.c=.o}
+
+${NAME}: ${OBJS}
+	ar -rsc ${NAME} ${OBJS} 
+
+all: ${NAME}
+
+clean:
+	rm *.o libft.a
+
+.PHONY: all
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(const char *s1, char const *set)
+int	ft_strncmp(const char *s1, char const *s2)
+{
+        int     i;
+
+        i = 0;
+        while (s1[i] || s2[i])
+        {
+                if (s1[i] != s2[i])
+                        return (s1[i] - s2[i]);
+                i++;
+        }
+        return (0);
+}
